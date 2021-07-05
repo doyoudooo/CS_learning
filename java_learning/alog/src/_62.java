ee856a2f33b45e17a1a184683703c3eb61ceea18
test <email>
2021-07-05 23:40:14 +0800

    提交笔记修改

---
+++ java_learning/alog/src/_62.java
@@ -0,0 +1,43 @@
+public class _62 {
+    /*
+     * @lc app=leetcode.cn id=62 lang=java
+     *
+     * [62] 不同路径
+     */
+
+    // @lc code=start
+
+        public int uniquePaths(int m, int n) {
+//1.要明确数组及其下标的含义
+            int [][]dp=new int[m+1][n+1];
+            dp[0][0]=1;
+            for (int i = 0; i < m; i++) {
+                dp[i][0]=1;
+            }
+            for (int j = 0; j < n; j++) {
+                dp[0][j]=1;
+            }
+
+//        2.初始化，递推公式，遍历顺序
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+
+            }
+            return dp[m-1][n-1];
+        }
+
+        public static void main(String[] args) {
+
+            _62 s1=new _62();
+            int a= 0;
+            a=s1.uniquePaths(1,1);
+            System.out.println(a);
+        }
+    }
+// @lc code=end
+
+
+
---
+++ java_learning/vscode_java_leetcode/63.不同路径-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=63 lang=python3
+#
+# [63] 不同路径 II
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        """"""
+# @lc code=end
+
+if __name__ == '__main__':
+    so1=Solution()
+    so1.uniquePathsWithObstacles([[0,1,0],[0,1,0],[0,0,0]])
+
+
